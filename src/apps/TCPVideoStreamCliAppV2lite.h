1c70679d2873c2d4159cee21a837718dbc45233f
KristianZE <38417354+KristianZE@users.noreply.github.com>
2023-04-29 11:03:51 +0200

    typo fix

--- src/apps/TCPVideoStreamCliAppV2lite.h
+++ src/apps/TCPVideoStreamCliAppV2lite.h
@@ -44,7 +44,7 @@ protected:
     int max_level = 5;
     int last_level = 0;
     long current_rate;
-    std::vector<int> rates = {41, 83, 178, 370, 750}; // In byte/s331
+    std::vector<int> rates = {41, 83, 178, 370, 750}; // In byte/s
     //std::vector<int> rates = {14, 42, 70, 197, 492}; // In byte/s
     int p = 3;
 
